5eaea0148a7353aa3aa5247807b3ee2047edcbc3
Abhaysinh8032 <113353038+Abhaysinh8032@users.noreply.github.com>
2023-11-16 21:42:54 +0530

    First Change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import Activation from "./components/Activation";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import NavBar from "./components/NavBar";
+import View from "./components/View";
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Activation/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/Signup" element={<Signup/>}/>
+        <Route path="/View" element={<View/>}/>
+        <Route path="*" element={<Activation/>}/>
+
+      </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
